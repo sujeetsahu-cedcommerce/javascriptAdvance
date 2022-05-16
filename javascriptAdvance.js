24d28e09e7a5dfb532d5f4f29d10b1841ca4b304
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-05-16 19:43:42 +0530

    Use objects of the above declared class to add employee details in to a JSON array object.

--- javascriptAdvance.js
+++ javascriptAdvance.js
@@ -1,3 +1,4 @@
++
 class Emp{
     constructor(id,name,salary){
         this.Id = id;
@@ -7,7 +8,23 @@ class Emp{
 }
 
 let emp1 = new Emp(101,"sujeet",35000);
+let emp2 = new Emp(102,"arvind",25000);
+let emp3 = new Emp(103,"akash",20000);
+
+jsn = JSON.stringify(emp1);
+displayJason(jsn);
+// console.log(jsn);
 // 
-text = "<table><tr><th>ID</th><th>Name</th><th>Salary</th></tr>"
- text +="<tr><td>"+ emp1.Id +"</td><td>" + emp1.Name + "</td><td>" + emp1.Salary +"</td></tr></table>"
-document.getElementById("display").innerHTML=text;
+// text = "<table><tr><th>ID</th><th>Name</th><th>Salary</th></tr>"
+//  text +="<tr><td>"+ emp1.Id +"</td><td>" + emp1.Name + "</td><td>" + emp1.Salary +"</td></tr></table>"
+// document.getElementById("display").innerHTML=text;
+
+
+// Use objects of the above declared class to add employee details in to a JSON array object.
+
+function displayJason(d){
+    data = JSON.parse(d);
+    txt = "<table><tr><th>ID</th><th>Name</th><th>Salary</th></tr>"
+    txt +="<tr><td>"+ data.Id +"</td><td>" + data.Name + "</td><td>" + data.Salary +"</td></tr></table>"
+   document.getElementById("displayJasonObject").innerHTML=txt;
+}
