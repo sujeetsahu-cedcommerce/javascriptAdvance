ae89d56c4d6d084bf9892ef8276a87a9280a30de
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-05-17 19:55:36 +0530

    Design an HTML page to take the employee details as input from the user and display them according to the given format attached.

--- javascriptAdvance.js
+++ javascriptAdvance.js
@@ -1,5 +1,124 @@
-+
-class Emp{
+const emp = [];
+
+function empDetails(){
+
+    // let text = "ID :<input id ='empID' type='text'><br>Name :<input id ='empName' type='text'><br>Basic Sal :<input id ='empSalary' type='text'><br><button id='addEmp'>Add</button>"
+
+    let emp_ID = document.getElementById("empID").value;
+
+    let emp_Name = document.getElementById("empName").value;
+
+    let emp_Sal = document.getElementById("empSalary").value;
+
+    const temp =[emp_ID,emp_Name,emp_Sal];
+
+    len=emp.length;
+
+    emp[len]=temp;
+
+    let text ="<table><tr><th>ID</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>";
+
+    for(var i=0;i<emp.length;i++)
+
+    {
+
+ 
+
+        text += "<tr id = '"+ i +"'>";
+
+        for(let j=0;j<=2;j++){
+
+            text +="<td>" + emp[i][j] + "</td>";
+
+        }
+
+        text +="<td><button id='Action' onclick=Delete_Emp(this)>Delete</button></td>";
+
+        text +="</tr>";
+
+    }
+
+    text +="</table>";
+
+    document.getElementById("display").innerHTML=text;
+
+    console.log(emp);
+
+    return text;
+
+}
+
+function Delete_Emp(obj){
+
+    var deltd =obj.closest('td');
+
+    var deltd_par = deltd.parentElement;
+
+    var index = deltd_par.id;
+
+    // console.log(deltd_par);
+
+    console.log(index);
+
+    emp.splice(index,1);
+
+    console.log(emp);
+
+    // document.getElementById("display").innerHTML=empDetails();
+
+    let text ="<table><tr><th>ID</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>";
+
+    for(var i=0;i<emp.length;i++)
+
+    {
+
+        for(let j=0;j<=2;j++){
+
+            text +="<td>" + emp[i][j] + "</td>";
+
+        }
+
+        text +="<td><button id='Action' onclick=Delete_Emp(this)>Delete</button></td>";
+
+        text +="</tr>";
+
+    }
+
+    text +="</table>";
+
+    document.getElementById("display").innerHTML=text;
+
+    console.log(emp);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Emp{
     constructor(id,name,salary){
         this.Id = id;
         this.Name = name;
@@ -12,7 +131,7 @@ let emp2 = new Emp(102,"arvind",25000);
 let emp3 = new Emp(103,"akash",20000);
 
 jsn = JSON.stringify(emp1);
-displayJason(jsn);
+displayJason(jsn); */
 // console.log(jsn);
 // 
 // text = "<table><tr><th>ID</th><th>Name</th><th>Salary</th></tr>"
@@ -22,9 +141,9 @@ displayJason(jsn);
 
 // Use objects of the above declared class to add employee details in to a JSON array object.
 
-function displayJason(d){
+/*function displayJason(d){
     data = JSON.parse(d);
     txt = "<table><tr><th>ID</th><th>Name</th><th>Salary</th></tr>"
     txt +="<tr><td>"+ data.Id +"</td><td>" + data.Name + "</td><td>" + data.Salary +"</td></tr></table>"
    document.getElementById("displayJasonObject").innerHTML=txt;
-}
+}*/
