7bea9cbec620b921291888d2b7d2b2785dca3b11
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-05-19 20:38:27 +0530

    Write a debug function and execute it whenever salary becomes less than or equals to zero.

--- arrowfunction.js
+++ arrowfunction.js
@@ -1,33 +1,33 @@
-const emp = [];
+// const emp = [];
 
-function empDetails(){
-    let emp_ID = document.getElementById("empID").value;
-    let emp_Name = document.getElementById("empName").value;
-    let emp_Sal = document.getElementById("empSalary").value;
-    renderEmpArrow(emp_ID ,emp_Name,emp_Sal);
-}
+// function empDetails(){
+//     let emp_ID = document.getElementById("empID").value;
+//     let emp_Name = document.getElementById("empName").value;
+//     let emp_Sal = document.getElementById("empSalary").value;
+//     renderEmpArrow(emp_ID ,emp_Name,emp_Sal);
+// }
 
-let renderEmpArrow = (emp_ID,emp_Name,emp_Sal) => {
-    const temp =[emp_ID,emp_Name,emp_Sal];
-    len=emp.length;
-    emp[len]=temp;
-    console.log(emp);
-    let text ="<table><th>ID</th><tr><th>Name</th><th>15% of Salary</th></tr>";
-    for(var i=0;i<emp.length;i++)
-    {
-        text += "<tr id = '"+ i +"'>";
-        for(let j=0;j<=2;j++){
-            if(j==2){
-                text +="<td>" + (emp[i][j]*15)/100 + "</td>";
-            }
-            else{
-                text +="<td>" + emp[i][j] + "</td>";
-            }
-        }
-       // text +="<td><button id='Action' onclick=Delete_Emp(this)>Delete</button></td>";
-        text +="</tr>";
-    }
-    text +="</table>";
-    document.getElementById("display").innerHTML=text;
-    return text;
-}
+// let renderEmpArrow = (emp_ID,emp_Name,emp_Sal) => {
+//     const temp =[emp_ID,emp_Name,emp_Sal];
+//     len=emp.length;
+//     emp[len]=temp;
+//     console.log(emp);
+//     let text ="<table><th>ID</th><tr><th>Name</th><th>15% of Salary</th></tr>";
+//     for(var i=0;i<emp.length;i++)
+//     {
+//         text += "<tr id = '"+ i +"'>";
+//         for(let j=0;j<=2;j++){
+//             if(j==2){
+//                 text +="<td>" + (emp[i][j]*15)/100 + "</td>";
+//             }
+//             else{
+//                 text +="<td>" + emp[i][j] + "</td>";
+//             }
+//         }
+//        // text +="<td><button id='Action' onclick=Delete_Emp(this)>Delete</button></td>";
+//         text +="</tr>";
+//     }
+//     text +="</table>";
+//     document.getElementById("display").innerHTML=text;
+//     return text;
+// }
--- javascriptAdvance.js
+++ javascriptAdvance.js
@@ -1,36 +1,44 @@
-// const emp = [];
+ var emp= [];
 
-// function empDetails(){
-//     let emp_ID = document.getElementById("empID").value;
-//     let emp_Name = document.getElementById("empName").value;
-//     let emp_Sal = document.getElementById("empSalary").value;
-//     //check(emp_ID,emp_Name,emp_Sal)
-// }
-// function render()
-// {
-//     let text ="<table><tr><th>ID</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>";
-//     for(var i=0;i<emp.length;i++)
-//     {
-//         text += "<tr id = '"+ i +"'>";
-//         for(let j=0;j<=2;j++){
-//             text +="<td>" + emp[i][j] + "</td>";
-//         }
-//         text +="<td><button id='Action' onclick=Delete_Emp(this)>Delete</button></td>";
-//         text +="</tr>";
-//     }
-//     text +="</table>";
-//     document.getElementById("display").innerHTML=text;
-//     return text;
-// }
-// function Delete_Emp(obj){
-//     var deltd =obj.closest('td');
-//     var deltd_par = deltd.parentElement;
-//     var index = deltd_par.id;
-//     console.log(index);
-//     emp.splice(index,1);
-//     console.log(emp);
-//     document.getElementById("display").innerHTML=renderEmpArrow;
-// }
+ function empDetails(){
+    let emp_ID = document.getElementById("empID").value;
+    let emp_Name = document.getElementById("empName").value;
+    let emp_Sal = document.getElementById("empSalary").value;
+    if(emp_Sal<=0){
+        debugfunction();
+    }
+    else{
+    check(emp_ID,emp_Name,emp_Sal);
+    }
+}
+
+function render()
+{
+    let text ="<table><tr><th>ID</th><th>Name</th><th>Total Salary</th><th>Action</th></tr>";
+    for(var i=0;i<emp.length;i++)
+    {
+        text += "<tr id = '"+ i +"'>";
+        for(let j=0;j<=2;j++){
+            text +="<td>" + emp[i][j] + "</td>";
+        }
+        text +="<td><button id='Action' onclick=Delete_Emp(this)>Delete</button></td>";
+        text +="</tr>";
+    }
+    text +="</table>";
+    document.getElementById("display").innerHTML=text;
+    return text;
+}
+
+function Delete_Emp(obj){
+    var deltd =obj.closest('td');
+    var deltd_par = deltd.parentElement;
+    var index = deltd_par.id;
+    console.log(index);
+    emp.splice(index,1);
+    console.log(emp);
+    document.getElementById("display").innerHTML=render();
+    // document.getElementById("display").innerHTML=renderEmpArrow;
+}
 
 
 /*Write a class to declare all the the details of the employee as mentioned in the above HTML page. Create an object of the class and display it.*/ 
@@ -80,36 +88,37 @@
 // Apply proper error handling (try catch) and calculate the salary of an employee as salary * 1.10 and display the employee details with updated salary.
 
 
-// function check(emp_ID,emp_Name,emp_Sal){
-//     try{
-//         if(emp_Sal <10000 || emp_Sal>40000) throw "salary must greater than 10000 and smaller than 40000";
-//         emp_Sal=emp_Sal*1.10;
-//         const temp =[emp_ID,emp_Name,emp_Sal];
-//         len=emp.length;
-//         emp[len]=temp;
-//         render();
-//      }
-//      catch(error){
-//          alert(error);
-//          document.getElementById("displayErr").innerHtml=error;
-//      } 
-// }
+function check(emp_ID,emp_Name,emp_Sal){
+    try{
+        if(emp_Sal <10000 || emp_Sal>40000) throw "salary must greater than 10000 and smaller than 40000";
+        emp_Sal=emp_Sal*1.10;
+        const temp =[emp_ID,emp_Name,emp_Sal];
+        len=emp.length;
+        emp[len]=temp;
+        render();
+     }
+     catch(error){
+        console.warn(error);
+        document.getElementById("displayErr").innerHtml=error;
+     } 
+}
 
 
 
-/* Create a separate JS file with a employee detail and import it to your main HTML/JS code to add and display this employee details in the HTML page. */
-
-import {id,name,salary} from "./import_export.js";
-        let text = id+" "+" "+name+" "+" "+salary;
-        console.log(text);
-
-        document.getElementById("displayImport").innerHTML=text; 
+/* Create a separate JS file with an employee detail and import it to your main HTML/JS code to add and display this employee details in the HTML page. */
 
 
+// import {id,name,salary} from "./import_export.js";
+//         let text = id+" "+" "+name+" "+" "+salary;
+//         console.log(text);
 
+//         document.getElementById("displayImport").innerHTML=text; 
+
+
+/*Write a debug function and execute it whenever salary becomes less than or equals to zero.*/
+
+function debugfunction(){
     
-
-
-
-
-
+    console.warn("salary never be zero");
+    debugger;
+}
